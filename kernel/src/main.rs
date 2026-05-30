3c1e82cc3560ce726de3be15aff0c36d453bad6c
Chloe Brett <chloebrett@users.noreply.github.com>
2026-05-30 22:03:22 +1000

    i am alive

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -9,11 +9,22 @@ global_asm!(include_str!("entry.S"));
 
 #[unsafe(no_mangle)]
 pub extern "C" fn kmain(_hart_id: usize, _dtb_phys: usize) -> ! {
-    loop {
-      unsafe {
-        asm!("wfi");
-      }
+  sbi_putchar(b'i');
+  sbi_putchar(b' ');
+  sbi_putchar(b'a');
+  sbi_putchar(b'm');
+  sbi_putchar(b' ');
+  sbi_putchar(b'a');
+  sbi_putchar(b'l');
+  sbi_putchar(b'i');
+  sbi_putchar(b'v');
+  sbi_putchar(b'e');
+  sbi_putchar(b'\n');
+  loop {
+    unsafe {
+      asm!("wfi");
     }
+  }
 }
 
 /// This function is called on panic.
@@ -24,4 +35,16 @@ fn panic(_info: &PanicInfo) -> ! {
         asm!("wfi");
       }
     }
+}
+
+fn sbi_putchar(c: u8) {
+  unsafe {
+    asm!(
+      "ecall",
+      in("a7") 1_usize, // legacy putchar EID
+      inout("a0") c as usize => _, // byte in; return clobbered
+      out("a1") _, // also clobbered by SBI
+      options(nostack),
+    )
+  }
 }
