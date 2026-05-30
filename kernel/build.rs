49c4bf330e443224af68db5def6449c5f2f140e4
Chloe Brett <chloebrett@users.noreply.github.com>
2026-05-30 21:39:15 +1000

    rerun

--- kernel/build.rs
+++ kernel/build.rs
@@ -2,4 +2,5 @@ fn main() {
   let dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
   println!("cargo:rustc-link-arg=-T{}/linker.ld", dir);
   println!("cargo:rerun-if-changed={}/linker.ld", dir);
+  println!("cargo:rerun-if-changed={}/src/entry.S", dir);
 }
