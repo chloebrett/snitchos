4bb442fcf0ae3d30ac416223aacc1d803803a6a4
Chloe Brett <chloebrett@users.noreply.github.com>
2026-05-30 21:24:05 +1000

    Implement asm linker.

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -2,10 +2,27 @@
 #![no_main]
 
 use core::panic::PanicInfo;
+use core::arch::global_asm;
+use core::arch::asm;
+
+global_asm!(r#"
+.section .text.boot
+.globl _start
+_start:
+  la sp, __stack_top
+  call kmain
+  # if kmain ever returns (it shouldn't), park
+1: wfi
+  j 1b
+"#);
 
 #[unsafe(no_mangle)]
-pub extern "C" fn _start() -> ! {
-    loop {}
+pub extern "C" fn kmain(_hart_id: usize, _dtb_phys: usize) -> ! {
+    loop {
+      unsafe {
+        asm!("wfi");
+      }
+    }
 }
 
 /// This function is called on panic.
