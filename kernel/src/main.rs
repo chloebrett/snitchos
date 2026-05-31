fd5f17ef56116892b18607d07048035dccbbbcb7
Chloe Brett <chloebrett@users.noreply.github.com>
2026-05-31 11:42:32 +1000

    Write macro

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -4,22 +4,14 @@
 use core::panic::PanicInfo;
 use core::arch::global_asm;
 use core::arch::asm;
+use core::fmt::Write;
 
 global_asm!(include_str!("entry.S"));
 
 #[unsafe(no_mangle)]
 pub extern "C" fn kmain(_hart_id: usize, _dtb_phys: usize) -> ! {
-  sbi_putchar(b'i');
-  sbi_putchar(b' ');
-  sbi_putchar(b'a');
-  sbi_putchar(b'm');
-  sbi_putchar(b' ');
-  sbi_putchar(b'a');
-  sbi_putchar(b'l');
-  sbi_putchar(b'i');
-  sbi_putchar(b'v');
-  sbi_putchar(b'e');
-  sbi_putchar(b'\n');
+  let _ = writeln!(&mut SbiConsole, "I am alive");
+
   loop {
     unsafe {
       asm!("wfi");
@@ -46,5 +38,16 @@ fn sbi_putchar(c: u8) {
       out("a1") _, // also clobbered by SBI
       options(nostack),
     )
+  }
+}
+
+pub struct SbiConsole;
+
+impl Write for SbiConsole {
+  fn write_str(&mut self, s: &str) -> core::fmt::Result {
+    for byte in s.bytes() {
+      sbi_putchar(byte);
+    }
+    Ok(())
   }
 }
